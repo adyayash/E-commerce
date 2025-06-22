d4ce6bf6532fba282297c59b16e141a4bd106485
adyayash <yasminbanu5112@gmail.com>
2025-06-22 14:47:05 +0530

    Order Service

---
+++ order_service/src/main/java/com/microservices/order_service/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.microservices.order_service.controller;
+
+
+import com.microservices.order_service.dto.OrderRequest;
+import com.microservices.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order_service/src/main/java/com/microservices/order_service/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.microservices.order_service.service;
+
+import com.microservices.order_service.dto.OrderRequest;
+import com.microservices.order_service.model.Order;
+import com.microservices.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+    }
+}
