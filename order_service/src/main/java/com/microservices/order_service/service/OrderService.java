fb9d134f755c1c0735c08cb0221a4f0fd278ccca
adyayash <yasminbanu5112@gmail.com>
2025-06-29 00:50:52 +0530

    Implemetation of Inter service communication

--- order_service/src/main/java/com/microservices/order_service/service/OrderService.java
+++ order_service/src/main/java/com/microservices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservices.order_service.service;
 
+import com.microservices.order_service.client.InventoryClient;
 import com.microservices.order_service.dto.OrderRequest;
 import com.microservices.order_service.model.Order;
 import com.microservices.order_service.repository.OrderRepository;
@@ -15,13 +16,20 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock){
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
